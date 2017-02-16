e146cbd9b14a0b768ff67586ccaecaeaff5c2980
Javier Poremski <javier@poremski.se>
2017-02-16 21:55:27 +0100

    Lade till appar: supply, production och delivery

--- krusty/settings.py
+++ krusty/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')yg4bi_&hpa!tjjcj*@in_q4#g1#*6o85ury!!)izska&*xf!x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    'stock',
+    'supply',
     'production',
     'delivery',
 ]
