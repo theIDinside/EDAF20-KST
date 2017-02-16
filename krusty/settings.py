bc1548c49476cd37418b5c71333b282b2d734f05
Javier Poremski <javier@poremski.se>
2017-02-16 21:10:33 +0100

    Uppdaterat databasen med tabeller: customers, ingredients; products; recipes; units.

--- krusty/settings.py
+++ krusty/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'krusty.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'krusty',
+        'USER': 'edaf20',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
